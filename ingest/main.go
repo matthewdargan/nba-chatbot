371549fdd5a135bcc499943326ef738ad360de9f
Matthew Dargan <matthewdargan57@gmail.com>
2024-07-09 11:04:32 -0500

    refactor(ingest): move main.go

--- ingest/main.go
+++ ingest/main.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Nba-chatbot generates embeddings for NBA statistics.
+// Ingest ingests NBA statistics and generates embeddings.
 package main
 
 import (
