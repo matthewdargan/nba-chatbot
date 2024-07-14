fc47efb78fa0dffe6cf4044f8eac8d53f61f13dd
Matthew Dargan <matthewdargan57@gmail.com>
2024-07-13 22:00:05 -0500

    docs(server): add dependency details

--- server/main.go
+++ server/main.go
@@ -3,6 +3,19 @@
 // license that can be found in the LICENSE file.
 
 // Server is an HTTP server for NBA statistics.
+//
+// The `/player-per-game` endpoint returns statistics for the nearest player
+// to the provided question.
+//
+// The [mxbai-embed-large] model generates embeddings for questions. The
+// server queries the PostgreSQL database for related embeddings and
+// statistical data. The PostgreSQL database requires the [pgvector] extension
+// to query embeddings. The [llama3] model generates responses to constructed
+// prompts.
+//
+// [llama3]: https://ollama.com/library/llama3
+// [mxbai-embed-large]: https://ollama.com/library/mxbai-embed-large
+// [pgvector]: https://github.com/pgvector/pgvector
 package main
 
 import (
